e495b8214dcc25bb41758bee2cc3392dedfd6e88
user <user@DESKTOP-TS570BQ>
2019-05-31 11:03:05 +0800

    一些改动

--- src/main/java/com/code/Application.java
+++ src/main/java/com/code/Application.java
@@ -1,21 +1,106 @@
 package com.code;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.Servlet;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @MapperScan(basePackages="com.code.dao")
 public class Application extends SpringBootServletInitializer{
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public ServletRegistrationBean<Servlet> druidServletRegister(@Qualifier("statViewServlet") Servlet servlet){
+		ServletRegistrationBean<Servlet> druidServlet = new ServletRegistrationBean<>();
+		druidServlet.setServlet(servlet);
+		druidServlet.setName("DruidStatViewServlet");
+		ArrayList<String> urlMapping = new ArrayList<>(1);
+		urlMapping.add("/druid/*");
+		druidServlet.setUrlMappings(urlMapping);
+		Map<String, String> initParams = new HashMap<>(2);
+		initParams.put("loginUsername", "123");
+		initParams.put("loginPassword", "123");
+		druidServlet.setInitParameters(initParams);
+		druidServlet.setEnabled(true);
+		druidServlet.setLoadOnStartup(2);
+		return druidServlet;
+	}
+	
+	@Bean
+	public FilterRegistrationBean<Filter> druidFilterRegister(@Qualifier("webStatFilter") Filter filter){
+		FilterRegistrationBean<Filter> druidFilter = new FilterRegistrationBean<>();
+		druidFilter.setFilter(filter);
+		druidFilter.addUrlPatterns("/*");
+		druidFilter.setName("DruidWebStatFilter");
+		druidFilter.setOrder(2);
+		Map<String, String> initParams = new HashMap<>(4);
+		initParams.put("exclusions", "/public/*,*.js,*.css,/druid*,*.jsp,*.swf");
+		initParams.put("principalSessionName", "sessionInfo");
+		initParams.put("profileEnable", "true");
+		druidFilter.setInitParameters(initParams);
+		return druidFilter;
+	}
+	
+	@Bean("statViewServlet")
+	public Servlet statViewServlet(){
+		return new StatViewServlet();
+	}
+	
+	@Bean("webStatFilter")
+	public Filter druidWebStatFilter(){
+		return new WebStatFilter();
+	}
+	
+	@Bean(destroyMethod="close")
+	public DruidDataSource dataSource(){
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setInitialSize(2);
+		dataSource.setMaxActive(50);
+		dataSource.setMinIdle(1);
+		dataSource.setMaxWait(10000);
+		dataSource.setValidationQuery("select 1 from dual");
+		dataSource.setTestOnBorrow(false);
+		dataSource.setTestWhileIdle(true);
+		dataSource.setQueryTimeout(6);
+		try {
+			dataSource.setFilters("stat");
+			dataSource.setFilters("wall");
+		} catch (SQLException e) {
+		}
+		return dataSource;
 	}
 
 }
--- src/main/java/com/code/controller/selectController.java
+++ src/main/java/com/code/controller/selectController.java
@@ -9,13 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.code.service.selectService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 
 @Controller
 @RequestMapping("/select")
+@Api("selectController相关的api")
 public class selectController extends BaseController{
 
 	private static Logger log = LoggerFactory.getLogger(selectController.class);
@@ -23,14 +28,17 @@ public class selectController extends BaseController{
 	@Autowired
 	private selectService selectService;
 	
-	@RequestMapping(value="/thisSelect")
+	
+	@ApiOperation(value = "查询表数据", notes = "查询数据库的表数据")
+    @ApiImplicitParam(name = "id", value = "ID", paramType = "path", required = true, dataType = "Integer")
+	@RequestMapping(value="/thisSelect", method = RequestMethod.POST)
 	@ResponseBody
 	public Map<String, Object> thisSelect() {
 		Map<String, Object> m = null;
 		Map<String, Object> params = new HashMap<>();
 		List<Map<String, Object>> list = selectService.thisSelect(params);
 		m = this.getResultMap(true, list, "查询成功！");
-		System.out.println(list);
+		log.debug("-->"+list);
 		return m;
 	}
 	
